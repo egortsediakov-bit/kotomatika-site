cf3b6c0943a4c87bd631dd1c327f575b1028d1e7
egortsediakov-bit <egortsediakov@gmail.com>
2026-09-09 23:35:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// После создания публичной функции в Yandex Cloud вставьте её URL сюда.
-// Пример: https://functions.yandexcloud.net/d4e********
-window.KOTOMATIKA_API_URL = "PASTE_YANDEX_FUNCTION_URL_HERE";
+window.KOTOMATIKA_API_URL = "https://functions.yandexcloud.net/d4ebffcjveejg2ek6smt";
